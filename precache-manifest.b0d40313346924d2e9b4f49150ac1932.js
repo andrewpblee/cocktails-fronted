b2d46da06f161edfc3b09e44ec64361db3a10b30
andrewpblee <andrewpblee@gmail.com>
2020-07-17 22:00:14 +0100

    Updates

---
+++ precache-manifest.b0d40313346924d2e9b4f49150ac1932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd944e5a8b93a2df332b7cdee195c4a1",
+    "url": "/cocktails-frontend/index.html"
+  },
+  {
+    "revision": "4954cedf7fe813e57b0e",
+    "url": "/cocktails-frontend/static/css/main.83e87208.chunk.css"
+  },
+  {
+    "revision": "ca2c8fedefb50f1b8067",
+    "url": "/cocktails-frontend/static/js/2.5f44c782.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails-frontend/static/js/2.5f44c782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4954cedf7fe813e57b0e",
+    "url": "/cocktails-frontend/static/js/main.42027a3c.chunk.js"
+  },
+  {
+    "revision": "3fa67533ff384a4e91c7",
+    "url": "/cocktails-frontend/static/js/runtime-main.e965666c.js"
+  },
+  {
+    "revision": "78f685e480fca76fb24c9630dfb46805",
+    "url": "/cocktails-frontend/static/media/placeholder.78f685e4.png"
+  }
+]);
